13a2f73b456691da654548c6f42743c2f5ee8b4b
breastroke423 <breastroke423@gmail.com>
2020-11-25 20:37:44 +0900

    flashcard update

--- flashcard.py
+++ flashcard.py
@@ -1,12 +1,4 @@
 # Excelから読み込んだ時の課題
-# ・クォーテーションマークを配列に入れるときに必ず配列に組み込むようにしないと、Excelベタ打ちが実現しない
-# ・Excel一行目は問題と質問の行列になっているから配列としては2行目から読み込んで配列へappendしないといけない
-# ↑現段階ではクォーテーション付きのExcelとなっている
-# ・ファイルのパスの位置がそれぞれのPCで必要
-# ↑外部入力をターミナルでできるように案内を書いて入力してもらって、という作業が必要
-# sheet名もかけるようにしておくべきかも。初期値としてはSheet1にしておいて、外部入力もできると汎用性が高まりそう
-
-
 
 # ランダム
 import random
@@ -16,16 +8,17 @@ from time import sleep
 import xlrd
 import pprint
 
-# 今はパスの位置は直打ちだが、将来構想としてはターミナルで入力できるようにしたい
-workbook = xlrd.open_workbook('C:\\Users\\81703\\Desktop\\qasheet.xlsx')
-sheet = workbook.sheet_by_name('Sheet1')
+# ファイルパス指定して作ってもらったどのファイルでも可能に
+inputFilePath = input('Excelファイルのパスを入力してください (Win：ファイル上で「Shift ＋ 右クリック から パスのコピー」/Mac：「⌘+option+C」): ')
+# ファイルパスコピーしてそのままペーストで使える
+
+workbook = xlrd.open_workbook(inputFilePath.strip('"'))
+inputSheetName = input('使用するシートを入力してください ： ')
+sheet = workbook.sheet_by_name(inputSheetName)
 
 # 問題文一覧を作成
 qArray = sheet.col_values(0)
 aArray = sheet.col_values(1)
-
-
-
 
 # ランダムな数字を生成
 numList = list(range(int(len(qArray))))
